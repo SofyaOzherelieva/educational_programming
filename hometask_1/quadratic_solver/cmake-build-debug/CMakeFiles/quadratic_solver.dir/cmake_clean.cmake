6d3a40cd085904f52ede3860c1796cc23c662f93
SofyaOzherelieva <sofya.ozherelieva@yandex.ru>
2018-09-20 16:12:50 +0300

    )

---
+++ hometask_1/quadratic_solver/cmake-build-debug/CMakeFiles/quadratic_solver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadratic_solver.dir/main.cpp.o"
+  "CMakeFiles/quadratic_solver.dir/test.cpp.o"
+  "CMakeFiles/quadratic_solver.dir/quadratic_solver.cpp.o"
+  "quadratic_solver.pdb"
+  "quadratic_solver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadratic_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
